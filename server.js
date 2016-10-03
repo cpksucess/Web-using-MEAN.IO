0579b979c275a0a12427bdfef46c91ad2329cf2d
cpksucess <cpksuccess@gmail.com>
2016-10-03 15:04:43 -0700

    add javaScript api route to the server

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
+app.get('/meows',function(req, res, next){
+	var meows =[
+		'Hello, Welcome',
+		'I am feeling good',
+		'How are you today',
+		'go outside and have '
+	];
+	res.send(meows);
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
