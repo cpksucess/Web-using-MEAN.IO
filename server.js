89073aa66a142925b18af9b3017ce6edd56317df
cpksucess <cpksuccess@gmail.com>
2016-10-03 23:43:47 -0700

    add tweets to the server
    Json, body-parser

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
+app.use(bodyParser.json());
 app.use(express.static('public'));
+
+var meows = [
+	"Hello, Welcome",
+	"I am feeling good",
+	"How are you today",	 
+];	
+
 app.get('/meows',function(req, res, next){
-	var meows =[
-		'Hello, Welcome',
-		'I am feeling good',
-		'How are you today',
-		'go outside and have '
-	];
+	
+	
 	res.send(meows);
-})
+});
+
+app.post('/meows',function(req, res, next){
+	meows.push(req.body.newMeow);
+	console.log(meows.length);
+	res.send();
+});
+
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
