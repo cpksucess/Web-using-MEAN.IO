c56a29b5b94ea9e376d2abfcca037844a1d3a56e
cpksucess <cpksuccess@gmail.com>
2016-10-04 23:37:45 -0700

    add Mongo db

--- server.js
+++ server.js
@@ -1,30 +1,39 @@
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
 var bodyParser = require('body-parser');
 var app = express();
+var db = null;
+
+MongoClient.connect("mongodb://localhost:27017/mittens", function(err, dbCon) {
+  if(!err) {
+    console.log("We are connected");
+    db = dbCon;
+  }
+});
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-var meows = [
-	"Hello, Welcome",
-	"I am feeling good",
-	"How are you today",	 
-];	
+
 
 app.get('/meows',function(req, res, next){
 	
-	
-	res.send(meows);
+	 db.collection('meows', function(err, meowsCol) {
+	 	meowsCol.find().toArray(function(err, meows) {
+	 		return res.json(meows);
+	 	});
+	 });
+	 
 });
 
 app.post('/meows',function(req, res, next){
-	meows.push(req.body.newMeow);
-	 
-	res.send();
+	db.collection('meows', function(err, meowsCol) {
+		var newMeow = {text: req.body.newMeow};
+	 	meowsCol.insert(newMeow, {w:1},function(err) {
+	 		return res.send();
+	 	});
+	 });
 });
-
-
-
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
